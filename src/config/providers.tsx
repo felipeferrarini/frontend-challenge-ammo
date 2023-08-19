8b1ea7b9cbbd99c1b9fd1119e3aeadc209db88d0
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2023-08-19 17:15:37 -0300

    feat: setup react query

--- src/config/providers.tsx
+++ src/config/providers.tsx
@@ -1,7 +1,16 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { useState, type PropsWithChildren } from 'react';
+import { createQueryClient } from './lib/query-client';
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
-}
+export const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(createQueryClient);
+
+  return (
+    <NextUIProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </NextUIProvider>
+  );
+};
